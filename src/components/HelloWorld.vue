7195be90ddc796d259325874c5cbe818268cecb3
Pedro Rodrigues <peaugust@hotmail.com>
2018-06-14 01:00:12 -0300

    inserting text area

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <input type="text" v-model="text">
-    <button @click="createPDF()">TESTE</button>
-    <pre>{{text}}</pre>
+    <h1>Insira os dados para criar um novo documento</h1>
+    <button @click="newSection()">ADD</button>
+    <div v-for="(section, index) in sections">
+      <p>Seção {{index}} </p>
+      <textarea type="text" v-model="section.text"></textarea>
+    </div>
+    <button @click="createPDF()">Send</button>
     <div id="test"></div>
   </div>
 </template>
@@ -14,15 +18,22 @@ export default {
   name: 'HelloWorld',
   data: function () {
     return {
-      text: ''
+      sections: [{text:''}]
     }
   },
   methods: {
+    newSection(){
+      this.sections.push({text: ''});
+    },
     createPDF () {
       var vm = this;
-      console.log(vm.text);
       var doc = new jsPDF();
-      doc.text(vm.text, 10, 10);
+      for(let i = 0; i < vm.sections.length; i++){
+        console.log(vm.sections[i].text);
+        doc.text(vm.sections[i].text, ((i+1)*10), ((i+1)*10));
+      }
+
+
       pdfobject.embed( doc.output('datauristring'), "#test");
 
     }
